052b734e72cf059b647a464b40b393367987cf7f
eetz1 <eetukah@metropolia.fi>
2015-11-18 14:26:51 +0200

    first

---
+++ src/stna/Stna.java
@@ -0,0 +1,21 @@
+/*
+ * eetz1s License such cool so wow
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stna;
+
+/**
+ *
+ * @author eetz1
+ */
+public class Stna {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
